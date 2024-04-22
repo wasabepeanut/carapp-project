a990530a69a98b84fe042ff9160412c3aa0cac94
wasabepeanut <wasabepeanut@gmail.com>
2024-04-22 19:45:26 +0300

    github.io deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/reactapp',
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
 })
