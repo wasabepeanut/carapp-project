9b71093d60117193494186ac3c3b9cdd0cd678c0
wasabepeanut <wasabepeanut@gmail.com>
2024-04-22 20:04:31 +0300

    base renamed

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/reactapp/',
+  base: '/carapp-project/',
   plugins: [react()],
   test: {
     globals: true,
