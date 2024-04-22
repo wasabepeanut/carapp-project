6cb332891f8629787cc8e0821e0b8c672889f28e
wasabepeanut <wasabepeanut@gmail.com>
2024-04-22 19:59:06 +0300

    deploying..

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/reactapp',
+  base: '/reactapp/',
   plugins: [react()],
   test: {
     globals: true,
